36f62f8265fe47627cc3b9d5c0835d80f24e2bbd
runios1 <77148127+runios1@users.noreply.github.com>
2022-11-16 13:07:32 +0200

    Update LastOfferJoinPolicy.cpp

--- LastOfferJoinPolicy.cpp
+++ LastOfferJoinPolicy.cpp
@@ -1,11 +1,10 @@
 #include "JoinPolicy.h"
 
-int JoinPolicy::Join(const vector<Agent>& offers,Simulation& s){
-    Graph g=s.getGraph();
-    Agent bestOffer(offers[offers.size()]);
+void JoinPolicy::Join(const vector<Coalition>& offers, int partyMandates){
+    Coalition bestOffer(offers[offers.size()]);
 
     // Creates a new agent in the new party in the coalition.
-    bestOffer.Duplicate(); //UPDATE ME 
-    
-    return g.getMandates(bestOffer.getPartyId());
+    Agent a=*bestOffer.getAgent();
+    Agent b=a.Duplicate(); //UPDATE ME 
+    bestOffer.JoinCoalition(b,partyMandates);
 }
