c16a9eb1c654c3c38102fe19c7ead565fb19be58
MichelleKoj <90860368+MichelleKoj@users.noreply.github.com>
2022-11-19 16:38:21 +0200

    Update Party.h

--- Party.h
+++ Party.h
@@ -20,23 +20,31 @@ class Party
 public:
     Party(int id, string name, int mandates, JoinPolicy *); 
 
+    //The Rule Of 5:
+    Party(const Party& other); //copy constructor
+    Party& operator = (const Party& other); //copy assignment
+    virtual ~Party(); //destructor
+    Party(Party&& other); //move constructor
+    Party& operator = (Party&& other); //move assignment
+
+
+
     State getState() const;
     void setState(State state);
     int getMandates() const;
     void step(Simulation &s);
     const string &getName() const;
-    
     int getIdOfParty() const;
     void addToOffers(Coalition& coalition);
     bool isInOffers(int coalitionId);
-    
+
 private:
     int mId;
     string mName;
     int mMandates;
     JoinPolicy *mJoinPolicy;
     State mState;
-
     int timer;
-    vector<Coalition> offers;
+    vector <Coalition> offers;
 };
+
