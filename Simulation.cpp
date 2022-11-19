a32f29a3b94e1ac08f3fb25df8ea79171a6a00d3
MichelleKoj <90860368+MichelleKoj@users.noreply.github.com>
2022-11-19 16:39:37 +0200

    Update Simulation.cpp

--- Simulation.cpp
+++ Simulation.cpp
@@ -2,12 +2,15 @@
 
 Simulation::Simulation(Graph graph, vector<Agent> agents) : mGraph(graph), mAgents(agents) 
 {
-   int i = 0;
+    int i = 0;
     for( Agent& a : agents){
         a.setCoalitionId(i);
         Coalitions.push_back(Coalition(mGraph.getMandates(a.getPartyId()), a, i));
         i++;
     }
+    // You can change the implementation of the constructor, but not the signature!
+    // we need to run on the vector of agents - for each agent create a coalition and push it to the vector of coalitions
+    // after the creation of the coalition - we need to update coalitionId of the agent
 }
 
 void Simulation::step()
@@ -27,7 +30,7 @@ bool Simulation::shouldTerminate() const
         if(mGraph.getParty(i).getState()!=State::Joined)
             allPartiesJoined=false;
     }
-    return allPartiesJoined;
+    return allPartiesJoined;    
 }
 
 const Graph &Simulation::getGraph() const
@@ -67,4 +70,61 @@ Graph &Simulation::getGraph()
     return mGraph;
 }
 
+//check:
+Party& Simulation::selectByEdgeWeight(int PartyId, int coalitionId){
+    int n = mGraph.getNumVertices()-1;
+int maxWeight = -1;
+Party* bestselect = &(mGraph.getParty(PartyId));
+for(int i=0; i < n; i++){
+    int edgeWeight = mGraph.getEdgeWeight(PartyId,i);
+    if(edgeWeight>0){
+        Party temp = getParty(i);
+        if(temp.getState()!=State::Joined){
+            if(edgeWeight > maxWeight){
+                if(temp.getState()==State::CollectingOffers){
+                    if(!temp.isInOffers(coalitionId)){
+                    maxWeight = edgeWeight;
+                    bestselect = &temp;
+                    }
+                }
+                else{
+                    //state Waiting
+                    maxWeight = edgeWeight;
+                    bestselect = &temp;
+                }
+            }
+        }
+    }
+}
+return (*bestselect);
+}
 
+Party& Simulation::selectByMandates(int PartyId, int coalitionId){
+
+int n =  mGraph.getNumVertices()-1;
+int maxMand = -1;
+Party* bestselect = &(mGraph.getParty(PartyId));
+
+for(int i=0; i < n; i++){
+    if(mGraph.getEdgeWeight(PartyId,i)>0){
+        Party temp = getParty(i);
+        if(temp.getState()!=State::Joined){
+            if(temp.getMandates() > maxMand){
+                if(temp.getState()==State::CollectingOffers){
+                    if(!temp.isInOffers(coalitionId)){
+                    maxMand = temp.getMandates();
+                    bestselect = &temp;
+                    }
+                }
+                else{
+                    //state Waiting
+                    maxMand = temp.getMandates();
+                    bestselect = &temp;
+                }
+            }
+        }
+    }
+}
+return (*bestselect);
+
+}
