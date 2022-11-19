fade4625c0776829b04bf72a7fd95d24573aacf2
MichelleKoj <90860368+MichelleKoj@users.noreply.github.com>
2022-11-19 09:32:07 +0200

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -29,3 +29,63 @@ Party &Graph::getParty(int partyId)
 {
     return mVertices[partyId];
 }
+
+const Party& Graph::selectByEdgeWeight(int PartyId, int coalitionId){
+
+int n = getNumVertices()-1;
+int maxWeight = -1;
+Party* bestselect = &getParty(PartyId);
+for(int i=0; i < n; i++){
+    int edgeWeight = getEdgeWeight(PartyId,i);
+    if(edgeWeight>0){
+        Party temp = getParty(i);
+        if(temp.getState()!=State::Joined){
+            if(edgeWeight > maxWeight){
+                if(temp.getState()==State::CollectingOffers){
+                    if(!temp.isInOffers(coalitionId)){
+                    maxWeight = edgeWeight;
+                    bestselect = &temp;
+                    }
+                }
+                else{
+                    //state Waiting
+                    maxWeight = edgeWeight;
+                    bestselect = &temp;
+                }
+            }
+        }
+    }
+}
+return (*bestselect);
+}
+
+
+const Party& Graph::selectByMandates(int PartyId, int coalitionId){
+
+int n = getNumVertices()-1;
+int maxMand = -1;
+Party* bestselect = &getParty(PartyId);
+
+for(int i=0; i < n; i++){
+    if(getEdgeWeight(PartyId,i)>0){
+        Party temp = getParty(i);
+        if(temp.getState()!=State::Joined){
+            if(temp.getMandates() > maxMand){
+                if(temp.getState()==State::CollectingOffers){
+                    if(!temp.isInOffers(coalitionId)){
+                    maxMand = temp.getMandates();
+                    bestselect = &temp;
+                    }
+                }
+                else{
+                    //state Waiting
+                    maxMand = temp.getMandates();
+                    bestselect = &temp;
+                }
+            }
+        }
+    }
+}
+return (*bestselect);
+
+}
