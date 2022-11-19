e48a007ef042f6c0d7ced16e7188333ab12fc219
MichelleKoj <90860368+MichelleKoj@users.noreply.github.com>
2022-11-19 16:29:23 +0200

    Update Agent.h

--- Agent.h
+++ Agent.h
@@ -12,11 +12,17 @@ class Agent
 public:
     Agent(int agentId, int partyId, SelectionPolicy *selectionPolicy);
     Agent(int agentId, int partyId, SelectionPolicy *selectionPolicy, int CoalitionId);
-    
+
+    //The Rule of 5:
+    Agent(const Agent& other); // copy constructor
+    Agent& operator = (const Agent& other); // copy assignment
+    virtual ~Agent(); // destructor
+    Agent(Agent&& other); // move constuctor
+    Agent& operator = (Agent&& other); // move assignment
+
     int getPartyId() const;
     int getId() const;
     void step(Simulation &);
-    
     Agent* Cloning(int partyid, int newAgentId, int CoalitionId);
     int getCoalitionId() const;
     void setCoalitionId(int coalitionId);
