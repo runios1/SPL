295211c166c23fef9c7b7de4adf4c54c81da1db6
MichelleKoj <90860368+MichelleKoj@users.noreply.github.com>
2022-11-19 16:36:40 +0200

    Update MandatesJoinPolicy.cpp

--- MandatesJoinPolicy.cpp
+++ MandatesJoinPolicy.cpp
@@ -14,7 +14,7 @@ void JoinPolicy::Join(const vector<Coalition>& offers,int partyMandates, int par
     }
     // Creates a new agent in the new party in the coalition.
     Agent a=*bestOffer.getAgent();
-    Agent *b=a.Cloning(partyid,newAgentid); //UPDATE ME 
+    Agent *b=a.Cloning(partyid,newAgentid,a.getCoalitionId()); 
     bestOffer.JoinCoalition(*b,partyMandates);
     //pay attention that b is a pointer to Agent varible on heap, make sure to find the right place to delete varible from heap
     //we need to check what happens to '*b' at the end of scope of Join
