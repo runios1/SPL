f98f569411440423dcf56ada936b6d1e63ef6a8b
MichelleKoj <90860368+MichelleKoj@users.noreply.github.com>
2022-11-21 00:40:39 +0200

    Update MandatesSelectionPolicy.cpp

--- MandatesSelectionPolicy.cpp
+++ MandatesSelectionPolicy.cpp
@@ -1,7 +1,7 @@
 #include "SelectionPolicy.h"
 #include "Simulation.h"
 
-Party& SelectionPolicy:: Select(int PartyId, int coalitionId,Simulation& sim){
+Party& MandatesSelectionPolicy:: Select(int PartyId, int coalitionId,Simulation& sim){
     Party& output = sim.selectByMandates(PartyId,coalitionId);
     return output;
 }
