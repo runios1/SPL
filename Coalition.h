b620bebeb739914d1facbc06de6e7b71b04945f0
runios1 <77148127+runios1@users.noreply.github.com>
2022-11-20 14:23:00 +0200

    Update Coalition.h

--- Coalition.h
+++ Coalition.h
@@ -1,6 +1,10 @@
 #pragma once
-#include "vector"
+#include <vector>
 #include "Agent.h"
+
+class Agent;
+
+using std::vector;
 
 class Coalition{
     public:
@@ -8,8 +12,10 @@ class Coalition{
         
         void JoinCoalition(Agent& duplicatedAgent,int partyMandates);
         Agent* getAgent();
+        Agent* getAgent(int agentId);
         int getMandates();
         int getId();
+        int getNumAgents() const;
 
     private:
         int mMandates;
