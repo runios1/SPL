63d097670e62623be71bc1987f730bbd1d263789
runios1 <77148127+runios1@users.noreply.github.com>
2022-11-22 19:09:44 +0200

    Update SelectionPolicy.h

--- SelectionPolicy.h
+++ SelectionPolicy.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "Party.h"
+#include <string>
 
 class Simulation;
+
+using std::string;
 
 class SelectionPolicy {
 public:
     virtual Party& Select(int PartyId, int coalitionId, Simulation& sim)=0;
+
+    virtual string getType()=0;
 
     virtual ~SelectionPolicy()=default;
 };
@@ -14,11 +19,11 @@ public:
 class MandatesSelectionPolicy: public SelectionPolicy{ 
     public:
     virtual Party& Select(int PartyId, int coalitionId,Simulation& sim);
-
+    virtual string getType();
 };
 
 class EdgeWeightSelectionPolicy: public SelectionPolicy{ 
     public:
     virtual Party& Select(int PartyId, int coalitionId,Simulation& sim);
-
+    virtual string getType();
 };
