18ce2cd912d33ecd71d18bdbfe3b00d2ee11b9d7
runios1 <77148127+runios1@users.noreply.github.com>
2022-11-22 19:02:00 +0200

    Update JoinPolicy.h

--- JoinPolicy.h
+++ JoinPolicy.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "Coalition.h"
+#include <string>
+
+using std::string;
 
 class JoinPolicy {
     public:
     virtual void Join(const vector <Coalition> & offers,int partyMandates, int partyid, int newAgentid)=0;
+    
+    virtual string getType()=0;
 
     virtual ~JoinPolicy()=default;
 };
@@ -11,9 +16,11 @@ class JoinPolicy {
 class MandatesJoinPolicy : public JoinPolicy {
 public:
     virtual void Join(const vector <Coalition> & offers,int partyMandates, int partyid,int newAgentid);
+    virtual string getType();
 };
 
 class LastOfferJoinPolicy : public JoinPolicy {
 public:
     virtual void Join(const vector <Coalition> & offers,int partyMandates, int partyid,int newAgentid);
+    virtual string getType();
 };
