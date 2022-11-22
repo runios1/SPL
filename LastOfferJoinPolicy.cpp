9f659916feae34a0ac384d4c63b579502c6e9c21
runios1 <77148127+runios1@users.noreply.github.com>
2022-11-22 19:05:26 +0200

    Update LastOfferJoinPolicy.cpp

--- LastOfferJoinPolicy.cpp
+++ LastOfferJoinPolicy.cpp
@@ -1,17 +1,24 @@
 #include "JoinPolicy.h"
 
 //virtual
-void LastOfferJoinPolicy::Join(const vector<Coalition>& offers, int partyMandates, int partyid, int newAgentid){
-    Coalition bestOffer(offers[offers.size()]);
+void MandatesJoinPolicy::Join(const vector<Coalition>& offers,int partyMandates, int partyid, int newAgentid){
+    int maxMend(-1);
+    Coalition bestOffer=offers[0];
 
+    for(Coalition c : offers){
+        int mandates(c.getMandates());
+        if(mandates>maxMend){
+            maxMend=mandates;
+            bestOffer=c;
+        }
+    }
     // Creates a new agent in the new party in the coalition.
     Agent a=*bestOffer.getAgent();
-    Agent *b=a.Cloning(partyid,newAgentid,a.getCoalitionId()); 
+    Agent* b=a.Cloning(partyid,newAgentid,a.getCoalitionId()); 
     bestOffer.JoinCoalition(*b,partyMandates);
-    //pay attention that b is a pointer to Agent varible on heap, make sure to find the right place to delete varible from heap
-    //we need to check what happens to '*b' at the end of scope of Join
-} 
+    delete b;
+}
 
-string LastOfferJoinPolicy::getType(){
+string MandatesJoinPolicy::getType(){
     return "L";
 }
