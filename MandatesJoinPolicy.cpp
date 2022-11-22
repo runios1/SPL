075250a70f29d10cf42e48ce9a46cac3856d9916
MichelleKoj <90860368+MichelleKoj@users.noreply.github.com>
2022-11-22 17:53:28 +0200

    Update MandatesJoinPolicy.cpp

--- MandatesJoinPolicy.cpp
+++ MandatesJoinPolicy.cpp
@@ -19,3 +19,7 @@ void MandatesJoinPolicy::Join(const vector<Coalition>& offers,int partyMandates,
     //pay attention that b is a pointer to Agent varible on heap, make sure to find the right place to delete varible from heap
     //we need to check what happens to '*b' at the end of scope of Join
 }
+
+string MandatesJoinPolicy::getType(){
+    return "M";
+}
