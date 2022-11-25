78ede29c0b049a29700eb860e62f2db7a7652b59
runios1 <77148127+runios1@users.noreply.github.com>
2022-11-25 23:33:50 +0200

    Update MandatesJoinPolicy.cpp

--- MandatesJoinPolicy.cpp
+++ MandatesJoinPolicy.cpp
@@ -1,21 +1,9 @@
 #include "JoinPolicy.h"
+#include "Simulation.h"
 
 //virtual
-void MandatesJoinPolicy::Join(vector<Coalition>& offers,int partyMandates, int partyid, int newAgentid){
-    int maxMend(-1);
-    Coalition& bestOffer=offers[0];
-
-    for(Coalition c : offers){
-        int mandates(c.getMandates());
-        if(mandates>maxMend){
-            maxMend=mandates;
-            bestOffer=c;
-        }
-    }
-    // Creates a new agent in the new party in the coalition.
-    Agent a=*bestOffer.getAgent();
-    Agent b=std::move(a.Cloning(partyid,newAgentid,a.getCoalitionId()));
-    bestOffer.JoinCoalition(b,partyMandates);
+void MandatesJoinPolicy::Join(Simulation sim,vector<int>& offers,int partyMandates, int partyid, int newAgentid){
+    sim.joinByMandates(offers,partyMandates, partyid, newAgentid);
 }
 
 string MandatesJoinPolicy::getType(){
